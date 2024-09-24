1db4cf37f71568a70985cd7a8156a89632b3378e
Rahul Jaiswal <rahuljaiswalacm@gmail.com>
2024-09-24 20:39:16 +0530

    Todo App completed Using NextJS

---
+++ app/api/route.js
@@ -0,0 +1,42 @@
+import { ConnectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/modals/TodoModel";
+import { NextResponse } from "next/server";
+
+const LoadDB = async () => {
+  await ConnectDB();
+};
+
+LoadDB();
+
+export async function GET(request) {
+    const todos = await TodoModel.find({});
+  return NextResponse.json({ todos:todos});
+}
+
+export async function POST(request) {
+  const { title, description } = await request.json();
+
+  await TodoModel.create({ title, description });
+
+  return NextResponse.json({ msg: "Todo Created" });
+}
+
+
+export async function DELETE(request) {
+  const mongoId = await request.nextUrl.searchParams.get('mongoId');
+  await TodoModel.findByIdAndDelete(mongoId);
+
+  return NextResponse.json({ msg: "Todo Delete" });
+}
+
+
+export async function PUT(request) {
+  const mongoId = await request.nextUrl.searchParams.get('mongoId');
+  await TodoModel.findByIdAndUpdate(mongoId, {
+    $set:{
+      isCompleted:true
+    }
+  });
+
+  return NextResponse.json({ msg: "Todo Completed" });
+}
--- app/page.js
+++ app/page.js
@@ -1,101 +1,127 @@
-import Image from "next/image";
+"use client";
+import Todo from "@/components/Todo";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [todoData, setTodoData] = useState([]);
+
+  const fetchTodos = async () => {
+    const response = await axios('/api');
+    setTodoData(response.data.todos)
+  }
+
+  const deleteTodo = async (id) =>{
+    const response = await axios.delete('/api', {
+      params:{
+        mongoId:id
+      }
+    });
+    toast.success(response.data.msg);
+    fetchTodos();
+  }
+
+  const completeTodo = async (id) => {
+    const response = await axios.put('/api', {}, {
+      params:{
+        mongoId:id
+      }
+    })
+    toast.success(response.data.msg);
+    fetchTodos();
+  }
+
+  useEffect(()=>{
+    fetchTodos();
+  },[])
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    console.log(formData);
+    const description = e.target.description;
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      // api code
+      const response = await axios.post("/api", formData);
+
+      toast.success(response.data.msg);
+      setFormData({
+        title: "",
+        description: "",
+      });
+      await fetchTodos();
+    } catch (error) {
+      toast.error("Glti ho gyi");
+    }
+  };
+
+  return (
+    <>
+      <ToastContainer theme="dark" />
+      <form
+        onSubmit={onSubmitHandler}
+        className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto"
+      >
+        <input
+          value={formData.title}
+          onChange={onChangeHandler}
+          type="text"
+          name="title"
+          placeholder="Enter Title"
+          className="px-3 py-2 border-2 w-full"
+        />
+        <textarea
+          value={formData.description}
+          onChange={onChangeHandler}
+          name="description"
+          placeholder="Enter Description"
+          className="px-3 py-2 border-2 w-full"
+        ></textarea>
+        <button type="submit" className="bg-orange-600 py-3 px-11 text-white">
+          Add Todo
+        </button>
+      </form>
+
+      <div className="relative overflow-x-auto mt-24 w-[70%] mx-auto border-2">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {todoData.map((item, index)=>{
+              return <Todo key={index} id={index} title={item.title} description={item.description} complete={item.isCompleted} mongoId={item._id} deleteTodo={deleteTodo} completeTodo={completeTodo} />
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
